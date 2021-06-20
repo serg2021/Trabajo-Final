b58f73ad87f0c8e7794e82b1cc7d1d3af8fd8354
serg2021 <sergio.sastre@edu.uah.es>
2021-06-20 22:42:29 +0200

    Testeos en directorios de Urban en Lasso, Linear y SVR

--- Código/ISA2 PPNIV Workshop/Regressors/SVM_regressor.m
+++ Código/ISA2 PPNIV Workshop/Regressors/SVM_regressor.m
@@ -1,22 +1,27 @@
 %%Train regressor - SVM
 clear all
 close all
-%hist_folder = 'histograms_full_set_3';
-hist_folder = 'histograms_full_set_3_Highway';
+
+%hist_folder = 'histograms_full_set_3_urban';
+%hist_folder = 'histograms_full_set_3_Highway';
+hist_folder = 'histograms_full_set_2_Urban';
 
 %load ('../ISA2_v1/Highway/speed_H1.mat')
 %load ('../ISA2_v1/Highway/speed_H2.mat')
-load ('../../Swiftnet/configs/out/ISA2/Highway/speed_H1.mat');
-load ('../../Swiftnet/configs/out/ISA2/Highway/speed_H2.mat');
+%load ('../../Swiftnet/configs/out/ISA2/Highway/speed_H1.mat');
+%load ('../../Swiftnet/configs/out/ISA2/Highway/speed_H2.mat');
+speed_u1 = load ('../../Swiftnet/configs/out/ISA2/Urban/speed_U1.mat');
+speed_u2 = load ('../../Swiftnet/configs/out/ISA2/Urban/speed_U2.mat');
+speed_u3 = load ('../../Swiftnet/configs/out/ISA2/Urban/speed_U3.mat');
 
-%hist_train = load(fullfile(hist_folder, 'histogramsautovia2.mat'));
-%hist_test = load(fullfile(hist_folder, 'histogramsautovia.mat'));
-hist_train = load(fullfile('../../Swiftnet/configs/out/Results/' ,hist_folder, 'histogramsH2.mat'));
-hist_test = load(fullfile('../../Swiftnet/configs/out/Results/', hist_folder, 'histogramsH1.mat'));
-X_train = hist_train.hist';
+%hist_train = [load(fullfile(hist_folder, 'histogramsseq1.mat')), load(fullfile(hist_folder, 'histogramsseq3.mat'))];
+%hist_test = load(fullfile(hist_folder, 'histogramsseq2.mat'));
+hist_train = [load(fullfile('../../Swiftnet/configs/out/Results/' ,hist_folder, 'histogramsU1.mat')),load(fullfile('../../Swiftnet/configs/out/Results/' ,hist_folder, 'histogramsU2.mat'))];
+hist_test = load(fullfile('../../Swiftnet/configs/out/Results/', hist_folder, 'histogramsU3.mat'));
+X_train = [hist_train.hist]';
 X_test = hist_test.hist';
-Y_train = speed_a2';
-Y_test = speed_a1';
+Y_train = [speed_u1.speed_2fps(1:end), speed_u2.speed_2fps(1:end)];
+Y_test = [speed_u3.speed_2fps(1:end)]';
 
 Mdl = fitrsvm(X_train,Y_train,'KernelFunction', 'linear', 'KernelScale', 'auto');
 YHat = predict(Mdl,X_test);
